df2d8ad9985ad73a59e2d3751d54e1a6e998915a
Jaydenxm419 <123033694+Jaydenxm419@users.noreply.github.com>
2024-09-14 20:34:06 -0400

    This was simply the most interesting code I have had to debug thus far. The syntax was twisting my brain around but WE GOT THERE. Array list functionalty was implemented and tested in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
-// This is the main file
+// main.cpp
+#include <iostream>
+#include "AssignmentOne/node.h" 
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main() {
+        using namespace std;
+        // File of items to be read into array
+        ifstream file("magicitems.txt");
+        // Items in file
+        string item;
+        // Items array that holds magic items from text file
+        vector<string> items;
+
+        // Check if the input file exists
+        if (file) {
+            // Iterate each line of the text file into the items array
+            while (getline(file, item)) {
+                items.push_back(item);
+            }
+            // Close the file to prevent errors
+            file.close();
+            // Retrieve the size of the array for manipulation
+            int n = items.size();
+            for (int i = 0; i < n; i++) {
+                Node* node = new Node(items[i]);
+                Node* nextNode = new Node(items[i + 1]);
+                node->setNext(nextNode);
+                std::cout << node->getData() << " -> ";
+                std::cout << nextNode->getData() << "\n";
+            }
+        // Handle error if the file doesn't exist
+}       else {
+            cerr << "ERROR: File may be open or does not exist!" << endl;
+            return 1;
+        }
+        return 0;
+}
+
