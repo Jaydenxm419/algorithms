6ff8b1248324bfa288a5ae0898d4f015763de5dd
Jaydenxm419 <123033694+Jaydenxm419@users.noreply.github.com>
2024-09-14 20:58:58 -0400

    That was a weird git issue. The commit was broken. I won't retype so this will be brief. Vector to an array. Blank space to nullptr.

--- main.cpp
+++ main.cpp
@@ -1,41 +1,67 @@
 // main.cpp
 #include <iostream>
-#include "AssignmentOne/node.h" 
+#include "AssignmentOne/node.h"
 #include <fstream>
 #include <string>
 #include <vector>
 
-int main() {
-        using namespace std;
-        // File of items to be read into array
-        ifstream file("magicitems.txt");
-        // Items in file
-        string item;
-        // Items array that holds magic items from text file
-        vector<string> items;
+int main()
+{
+    using namespace std;
+    // File of items to be read into array
+    ifstream file("magicitems.txt");
+    // Items in file
+    string item;
+    // Items array that holds magic items from text file
+    int arrSize = 666;
+    string items[arrSize];
 
-        // Check if the input file exists
-        if (file) {
-            // Iterate each line of the text file into the items array
-            while (getline(file, item)) {
-                items.push_back(item);
-            }
-            // Close the file to prevent errors
-            file.close();
-            // Retrieve the size of the array for manipulation
-            int n = items.size();
-            for (int i = 0; i < n; i++) {
-                Node* node = new Node(items[i]);
-                Node* nextNode = new Node(items[i + 1]);
-                node->setNext(nextNode);
-                std::cout << node->getData() << " -> ";
-                std::cout << nextNode->getData() << "\n";
-            }
-        // Handle error if the file doesn't exist
-}       else {
-            cerr << "ERROR: File may be open or does not exist!" << endl;
-            return 1;
+    // Check if the input file exists
+    if (file)
+    {
+        // Iterate each line of the text file into the items array
+        int i = 0;
+        while (getline(file, item))
+        {
+            items[i] = item;
+            i++;
         }
-        return 0;
+        // Close the file to prevent errors
+        file.close();
+        // Iterrate through the array and add the elements to the linked list
+        for (int i = 0; i < arrSize; i++)
+        {
+            // Declare the previous node
+            Node *node = new Node(items[i]);
+            // Ensure array overflow is resolved
+            if (i < arrSize - 1)
+            {
+                Node *nextNode = new Node(items[i + 1]);
+                node->setNext(nextNode);
+            }
+            // Set a null pointer at the end of the linked list
+            else
+            {
+                node->setNext(nullptr);
+            }
+            // Print the previous element and next element
+            if (node->getNext() != nullptr)
+            { 
+                std::cout << node->getData() << " -> ";
+                std::cout << node->getNext()->getData() << "\n"; // Print next node's data
+            }
+            else
+            {
+                std::cout << node->getData() << " -> ";
+                std::cout << "nullptr\n"; // No next node, print nullptr
+            }
+        }
+        // Handle error if the file doesn't exist
+    }
+    else
+    {
+        cerr << "ERROR: File may be open or does not exist!" << endl;
+        return 1;
+    }
+    return 0;
 }
-
