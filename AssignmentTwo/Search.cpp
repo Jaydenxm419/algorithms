004a2e8662094d39f2b29e0734239f64f6188ef3
Jaydenxm419 <123033694+Jaydenxm419@users.noreply.github.com>
2024-10-12 14:03:31 -0400

    Just some comments for doBinarySearch()

--- AssignmentTwo/Search.cpp
+++ AssignmentTwo/Search.cpp
@@ -43,30 +43,33 @@ void Search::doBinarySearch(std::string *items, Stack &stack, int size) {
     double comparisonCount = 0; // Comparison counter for each search
     double totalComparisons = 0; // Total number of comparisons for all searches
     double averageComparisons = 0; // Hold the average number of comparisons for all searches
-    int midpoint = size / 2;
+    int midpoint = 0; 
     int startIndex = 0;
-    int endIndex = size - 1;
+    int endIndex = 0;
     bool found = false;
+    // Search for all items in the stack
     while(!stack.isEmpty()) {
-        std::string item = stack.pop();
-        midpoint = size / 2;
-        startIndex = 0;
-        endIndex = size - 1;
-        found = false;
-        comparisonCount = 0;
+        std::string item = stack.pop(); // Get the next item
+        // Keep track of the focused portion of the array
+        midpoint = size / 2; 
+        startIndex = 0; 
+        endIndex = size - 1; 
+        found = false; // By default, the item is not found
+        comparisonCount = 0; // Counter to keep track of comparisons
+        // Continuously halve the array until the end index is greater than the start index
         while(!found && startIndex <= endIndex) {
-            midpoint = startIndex + (endIndex - startIndex) / 2; 
-            comparisonCount++;
-            if (item == items[midpoint]) {
+            midpoint = startIndex + (endIndex - startIndex) / 2; // Calculate the new midpoint
+            comparisonCount++; 
+            if (item == items[midpoint]) { // Item was found
                 std::cout << "Item: " << item << " found at index: " << midpoint << "\n";
                 found = true;
-            } else if (item < items[midpoint]) {
+            } else if (item < items[midpoint]) { // Focus on the left portion
                 endIndex = midpoint - 1;
-            } else if (item > items[midpoint]) {
+            } else if (item > items[midpoint]) { // Foucus on the right portion
                 startIndex = midpoint + 1;
             }
         }
-        totalComparisons += comparisonCount;
+        totalComparisons += comparisonCount; // Update total comparisons
     }
     std::cout << "--------------------------------------" << "\n";
     std::cout << "Total Comparisons: " << totalComparisons << "\n";
