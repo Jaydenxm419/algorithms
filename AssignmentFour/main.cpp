1e2e7d41b9f80963e3db607d6f1ebf5bf86165ab
Jaydenxm419 <123033694+Jaydenxm419@users.noreply.github.com>
2024-11-18 11:07:50 -0500

    Basic Parsing for spice commands

--- AssignmentFour/main.cpp
+++ AssignmentFour/main.cpp
@@ -1,12 +1,42 @@
 #include "Spice.h"
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 
 using namespace std;
 
 string SPICE_FILE = "AssignmentFour/spice.txt";
+
+// Split a string based on a delimiter
+std::vector<std::string> splitString(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::stringstream ss(str);
+    std::string token;
+
+    while (std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+// Create a new spice object
+void newSpice(std::string instructions) {
+    // Character that is being split on
+    char delimeter = ';';
+    // Vector for the line
+    std::vector<std::string> splitInstructions = splitString(instructions, delimeter);
+
+    for (int i = 0; i < splitInstructions[i].size(); i++) {
+        std::vector<std::string> splitLineInstructions = splitString(splitInstructions[i], ' ');
+        // for (int j = 0; j < splitLineInstructions[j].size(); j++) {
+            std::cout << splitInstructions[i];
+        // }
+    }
+    
+}
 
 // Remove comments from the vector
 std::vector<std::string> doIgnoreComments(std::vector<std::string> lines) {
@@ -53,9 +83,13 @@ std::vector<std::string> readSpiceFile() {
 void completeSpiceExercise() {
     // Get the spice instructions
     std::vector<std::string> lines = readSpiceFile();
-    // Print the ouput
+    std::string spiceSubStr = "spice";
+    std::string knapsackSubStr = "knapsack";
     for (int i = 0; i < lines.size(); i++) {
-        std::cout << lines[i] << "\n";
+        if (lines[i].find("spice") != std::string::npos) {
+           newSpice(lines[i]);
+           std::cout << std::endl;
+        } 
     }
 }
 
