4119fd2ef5cc58be7afde5d7321f04068437456e
Jaydenxm419 <123033694+Jaydenxm419@users.noreply.github.com>
2024-11-25 21:14:48 -0500

    Main file

--- AssignmentFour/main.cpp
+++ AssignmentFour/main.cpp
@@ -2,16 +2,34 @@
 #include "include/Knapsack.h"
 #include "include/SpiceHeist.h"
 #include "include/Sorting.h"
+#include <vector>
 #include <string>
 using namespace std;
+
+
+#include "include/DirectedGraph.h"
+#include "include/DirectedGraphVertex.h"
+#include "include/DirectedGraphEdge.h"
+#include "include/ParseHeistFile.h"
+#include "include/BuildDirectedGraph.h"
+#include "include/PrintDirectedGraph.h"
+#include "include/ProjectConstants.h"
+#include <iostream>
 
 // Contains steps for the spice hesit
 SpiceHeist doHeist;
 // Sorts the spices
 Sorting sorting;
 
-const string SPICE_KEY_WORD = "spice";
-const string KNAPSACK_KEY_WORD = "knapsack";
+// Complete the directed graph exercise
+void completeDirectedGraphExerice() {
+    // Read the file
+    vector<string> contents = parse.readHeistFile(DIRECTED_GRAPH_FILE);
+    // Build the graphs
+    vector<DirectedGraph*> graphs = build.buildGraph(contents);
+    // Build the adjacency list
+    print.printAdjancencyList(graphs);
+}
 
 // Complete the spice task
 void completeSpiceExercise() {
@@ -23,12 +41,12 @@ void completeSpiceExercise() {
     vector<Knapsack*> knapsacks = doHeist.getKnapsacks(KNAPSACK_KEY_WORD);
     // Calculate the greatest value for each knapsack
     doHeist.getHighestValue(sortedSpices, knapsacks);
-
 }
 
 // Assignment 4
 int main() {
     // Complete the spice task
-    completeSpiceExercise();
+    // completeSpiceExercise();
+    completeDirectedGraphExerice();
     return 0;
 }
